fc531fdb8002f9f29d76956eca8b6159f7a9dce7
kimyoungyin <yin199859@gmail.com>
2023-08-18 14:25:56 +0900

    feat, docs: sort를 통한 배열, 벡터 정렬

--- README.md
+++ README.md
@@ -278,3 +278,65 @@ cout << "원본 배열 1 인덱스 값: " << v[1] << "\n";
 return 0;
 }
 ```
+
+## sort(): 배열 혹은 벡터 정렬
+
+```cpp
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    // 컨테이너들의 요소를 정렬하기: sort
+    // sort(): 보통 array or vector 정렬 시에 쓰임. O(nlogn)
+    // sort(first, last, *커스텀비교함수)
+    // - 커스텀비교함수
+    // 0. 기본은 오름차순(커스텀비교함수를 넣지 않았을 때)
+    // 1. greater<타입>()을 넣어 내림차순 변경 가능
+    // 2. less<타입>()을 넣어 오름차순 정렬
+    cout << "배열 정렬"
+         << "\n";
+    int b[5];
+    for (int i = 0; i < 5; i++) {
+        b[4 - i] = i;
+    }
+    for (int i = 0; i < 5; i++)
+        cout << b[i] << " ";
+    cout << "\n";
+    // 오름차순
+    sort(b, b + 5);
+    for (int i = 0; i < 5; i++)
+        cout << b[i] << " ";
+    cout << "\n";
+    // 내림차순
+    sort(b, b + 5, greater<int>());
+    for (int i = 0; i < 5; i++)
+        cout << b[i] << " ";
+    cout << "\n\n";
+
+    cout << "벡터 정렬"
+         << "\n";
+    vector<int> v;
+    for (int i = 5; i >= 1; i--)
+        v.push_back(i);
+    for (int i : v)
+        cout << i << " ";
+    cout << "\n";
+    sort(v.begin(), v.end());
+    for (int i : v)
+        cout << i << " ";
+    cout << "\n";
+    sort(v.begin(), v.end(), greater<int>());
+    for (int i : v)
+        cout << i << " ";
+    cout << "\n";
+    return 0;
+    // 배열 정렬
+    // 4 3 2 1 0
+    // 0 1 2 3 4
+    // 4 3 2 1 0
+
+    // 벡터 정렬
+    // 5 4 3 2 1
+    // 1 2 3 4 5
+    // 5 4 3 2 1
+}
+```
--- a.cpp
+++ a.cpp
@@ -1,33 +1,85 @@
 #include <bits/stdc++.h>
 using namespace std;
+// c/cpp는 함수 내 함수 선언은 불가하다.
+// vector<pair<int, int>>를 "내림차순:오름차순"으로 정렬하고 싶다면
+bool cmp(pair<int, int> a, pair<int, int> b) { return a.first > b.first; }
+
 int main() {
-    vector<int> v{1, 2, 3};
-    vector<int> ret(3);
-    // copy: 벡터, 배열 모두 깊은 복사 가능
-    // copy(원본 시작 iterator, 원본 끝 이터레이터(마지막 + 1), 복사받는 시작
-    // 이터레이터);
-    // 1. 벡터 copy
-    copy(v.begin(), v.end(), ret.begin());
-    for (int i : ret)
+    // 컨테이너들의 요소를 정렬하기: sort
+    // sort(): 보통 array or vector 정렬 시에 쓰임. O(nlogn)
+    // sort(first, last, *커스텀비교함수)
+    // - 커스텀비교함수
+    // 0. 기본은 오름차순(커스텀비교함수를 넣지 않았을 때)
+    // 1. greater<타입>()을 넣어 내림차순 변경 가능
+    // 2. less<타입>()을 넣어 오름차순 정렬
+    cout << "배열 정렬"
+         << "\n";
+    int b[5];
+    for (int i = 0; i < 5; i++) {
+        b[4 - i] = i;
+    }
+    for (int i = 0; i < 5; i++)
+        cout << b[i] << " ";
+    cout << "\n";
+    // 오름차순
+    sort(b, b + 5);
+    for (int i = 0; i < 5; i++)
+        cout << b[i] << " ";
+    cout << "\n";
+    // 내림차순
+    sort(b, b + 5, greater<int>());
+    for (int i = 0; i < 5; i++)
+        cout << b[i] << " ";
+    cout << "\n\n";
+
+    cout << "벡터 정렬"
+         << "\n";
+    vector<int> v;
+    for (int i = 5; i >= 1; i--)
+        v.push_back(i);
+    for (int i : v)
         cout << i << " ";
     cout << "\n";
-    ret[1] = 100;
-    cout << "변경된 사본 벡터 1 인덱스 값: " << ret[1] << "\n";
-    cout << "원본 벡터 1 인덱스 값: " << v[1] << "\n";
-
-    // 2. 배열
-    const int ARRAY_N = 3;
-    int a[ARRAY_N] = {1, 2, 3};
-    int retA[ARRAY_N];
-    // copy(복사당할 배열 이름(시작 이터레이터), 복사당할 배열 끝 + 1, 복사할
-    // 배열 이름(시작))
-    copy(a, a + ARRAY_N, retA);
-    for (int i : a)
+    sort(v.begin(), v.end());
+    for (int i : v)
         cout << i << " ";
     cout << "\n";
-    retA[1] = 10;
-    cout << "변경된 사본 배열 1 인덱스 값: " << retA[1] << "\n";
-    cout << "원본 배열 1 인덱스 값: " << v[1] << "\n";
+    sort(v.begin(), v.end(), greater<int>());
+    for (int i : v)
+        cout << i << " ";
+    cout << "\n\n";
 
+    cout << "pair 기반 벡터"
+         << "\n";
+    vector<pair<int, int>> pairV;
+    for (int i = 10; i >= 1; i--)
+        pairV.push_back({i, 10 - i});
+    for (pair<int, int> i : pairV)
+        cout << i.first << ":" << i.second << " ";
+    cout << "\n";
+    sort(pairV.begin(), pairV.end());
+    for (auto i : pairV)
+        cout << i.first << ":" << i.second << " ";
+    cout << "\n";
+
+    // {내림차순, 오름차순} 하고 싶다면?: cmp 함수를 만들어 넣자
+    sort(pairV.begin(), pairV.end(), cmp);
+    for (auto i : pairV)
+        cout << i.first << ":" << i.second << " ";
+    cout << "\n";
     return 0;
+    // 배열 정렬
+    // 4 3 2 1 0
+    // 0 1 2 3 4
+    // 4 3 2 1 0
+
+    // 벡터 정렬
+    // 5 4 3 2 1
+    // 1 2 3 4 5
+    // 5 4 3 2 1
+
+    // pair 기반 벡터
+    // 10:0 9:1 8:2 7:3 6:4 5:5 4:6 3:7 2:8 1:9
+    // 1:9 2:8 3:7 4:6 5:5 6:4 7:3 8:2 9:1 10:0
+    // 10:0 9:1 8:2 7:3 6:4 5:5 4:6 3:7 2:8 1:9
 }
