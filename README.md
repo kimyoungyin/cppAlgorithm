29fd33602ecf4dc2be3c8030506f5723b7685913
kimyoungyin <yin199859@gmail.com>
2023-08-18 16:22:34 +0900

    feat, docs: accumulate(), max_element(), min_element()로 배열의 합, 최대값, 최소값 얻기

--- README.md
+++ README.md
@@ -411,3 +411,39 @@ int main() {
     // 5
 }
 ```
+
+## `accumulate()`, `max_element()`, `min_element()`: 배열에서 합, 최대값, 최소값 얻기
+```cpp
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    vector<int> v = {1, 2, 3, 4, 5, 10, 9, 8, 7, 6};
+    for (int i : v)
+        cout << i << " ";
+    cout << "\n";
+    // 합 = accumulate(시작, 끝, 초기값)
+    int sum = accumulate(v.begin(), v.end(), 0);
+    cout << sum << "\n";
+
+    // 최대값 이터레이터 = max_element(시작, 끝)
+    int a = *max_element(v.begin(), v.end()); // 최대값
+    auto b = max_element(v.begin(), v.end()); // 이터레이터
+    cout << "최대값은 " << a << "\n";
+    // 이터레이터 - 이터레이터 = 인덱스
+    cout << "최대값의 인덱스는 " << b - v.begin() << "\n";
+
+    // 최소값 이터레이터 = min_element(시작, 끝)
+    int c = *min_element(v.begin(), v.end()); // 최대값
+    auto d = min_element(v.begin(), v.end()); // 이터레이터
+    cout << "최소값은 " << c << "\n";
+    // 이터레이터 - 이터레이터 = 인덱스
+    cout << "최소값의 인덱스는 " << d - v.begin() << "\n";
+    return 0;
+    // 1 2 3 4 5 10 9 8 7 6
+    // 55
+    // 최대값은 10
+    // 최대값의 인덱스는 5
+    // 최소값은 1
+    // 최소값의 인덱스는 0
+}
+```
--- a.cpp
+++ a.cpp
@@ -1,24 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    // 정렬된 배열에서 어떤 값이 나오는 첫 번째 지점 혹은 초과하는 지점의 위치를
-    // 찾으려면
-    // lower_bound(처음, 끝, 찾는 값): 어떤 값이 나오는 첫 번째 지점
-    // 이터레이터 반환
-    // upper_bound(처음, 끝, 찾는 값): 어떤 값이 초과하는 첫
-    // 번째 지점 이터레이터 반환
-    vector<int> v{1, 3, 5, 3, 2, 2, 5};
-    sort(v.begin(), v.end());
-    cout << "정렬된 배열"
-         << "\n";
+    vector<int> v = {1, 2, 3, 4, 5, 10, 9, 8, 7, 6};
     for (int i : v)
         cout << i << " ";
     cout << "\n";
-    cout << lower_bound(v.begin(), v.end(), 3) - v.begin() << "\n";
-    cout << upper_bound(v.begin(), v.end(), 3) - v.begin() << "\n";
+    // 합 = accumulate(시작, 끝, 초기값)
+    int sum = accumulate(v.begin(), v.end(), 0);
+    cout << sum << "\n";
+
+    // 최대값 이터레이터 = max_element(시작, 끝)
+    int a = *max_element(v.begin(), v.end()); // 최대값
+    auto b = max_element(v.begin(), v.end()); // 이터레이터
+    cout << "최대값은 " << a << "\n";
+    // 이터레이터 - 이터레이터 = 인덱스
+    cout << "최대값의 인덱스는 " << b - v.begin() << "\n";
+
+    // 최소값 이터레이터 = min_element(시작, 끝)
+    int c = *min_element(v.begin(), v.end()); // 최대값
+    auto d = min_element(v.begin(), v.end()); // 이터레이터
+    cout << "최소값은 " << c << "\n";
+    // 이터레이터 - 이터레이터 = 인덱스
+    cout << "최소값의 인덱스는 " << d - v.begin() << "\n";
     return 0;
-    // 정렬된 배열
-    // 1 2 2 3 3 5 5
-    // 3
-    // 5
+    // 1 2 3 4 5 10 9 8 7 6
+    // 55
+    // 최대값은 10
+    // 최대값의 인덱스는 5
+    // 최소값은 1
+    // 최소값의 인덱스는 0
 }
