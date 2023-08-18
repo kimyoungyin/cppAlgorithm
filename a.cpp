151b865910e088f2f2caded226d99503ded91476
kimyoungyin <yin199859@gmail.com>
2023-08-18 11:15:59 +0900

    feat: memcpy로 배열을 깊은 복사

--- a.cpp
+++ a.cpp
@@ -1,19 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAX_N = 50;
-int a[MAX_N];
-int b[MAX_N][MAX_N];
 int main() {
-    memset(a, -1, sizeof(a));
-    memset(b, 0, sizeof(b));
-    for (int i = 0; i < MAX_N; i++)
-        cout << a[i] << " ";
-    cout << "\n";
-    for (int i = 0; i < MAX_N; i++) {
-        for (int j = 0; j < MAX_N; j++) {
-            cout << b[i][j] << " ";
-        }
-        cout << "\n";
-    }
+    int v[3] = {1, 2, 3};
+    int ret[3];
+    // memcpy: Array만 깊은 복사 가능
+    // void memcpy(복사할 배열 이름(포인터), 원본 배열 이름(포인터), 복사할
+    // 데이터의 길이)
+    memcpy(ret, v, sizeof(v));
+    cout << ret[1] << "\n";
+    ret[1] = 100;
+    cout << "복사하고 변경된 사본: " << ret[1] << "\n";
+    cout << "원본은 그대로인 " << v[1] << "\n";
     return 0;
 }
