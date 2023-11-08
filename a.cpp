e4b91001bc6f5be8bcca5374918b0f8d21a437b4
kimyoungyin <yin199859@gmail.com>
2023-11-08 16:59:05 +0900

    순열 with 재귀함수

--- a.cpp
+++ a.cpp
@@ -1,16 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-// 1. 타입으로 선언
-vector<vector<int>> v;
-// 2. 타입으로 선언 후 초기화까지
-vector<vector<int>> v2(10, vector<int>(10, 0));
-// 3. 1차원만 생성 후 이후 요소를 빈 배열로 채우기
-vector<int> v3[10];
-int main() {
-    for (int i = 0; i < 10; i++) {
-        // 빈 배열로 요소를 채우기
-        vector<int> temp;
-        v.push_back(temp);
+vector<int> v = {1, 5, 100, 3, 7};
+
+void printV(vector<int> v, int r) {
+    for (int i = 0; i < r; i++) {
+        cout << v[i] << " ";
     }
+    cout << '\n';
+}
+void p(int n, int r, int depth = 0) {
+    if (r == depth) {
+        printV(v, r);
+        return;
+    }
+
+    for (int i = depth; i < n; i++) {
+        swap(v[i], v[depth]);
+        p(n, r, depth + 1);
+        swap(v[i], v[depth]);
+    }
+    return;
+}
+
+int main() {
+    p(5, 2);
     return 0;
 }
