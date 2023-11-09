9f282725f55aeb8d7ae8c25b78344348f3fe17ea
kimyoungyin <yin199859@gmail.com>
2023-11-09 11:30:09 +0900

    10808

--- a.cpp
+++ a.cpp
@@ -1,28 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> v = {1, 5, 100, 3, 7};
-
-void printV(vector<int> v, int r) {
-    for (int i = 0; i < r; i++) {
-        cout << v[i] << " ";
-    }
-    cout << '\n';
-}
-void p(int n, int r, int depth = 0) {
-    if (r == depth) {
-        printV(v, r);
-        return;
-    }
-
-    for (int i = depth; i < n; i++) {
-        swap(v[i], v[depth]);
-        p(n, r, depth + 1);
-        swap(v[i], v[depth]);
-    }
-    return;
-}
-
+int answer[26];
+string str;
+// https://www.acmicpc.net/problem/10808
 int main() {
-    p(5, 2);
+    fill(answer, answer + 26, 0);
+    cin >> str;
+    for (char c : str)
+        answer[(int)c - 97]++;
+    for (int i : answer)
+        cout << i << " ";
+    cout << "\n";
     return 0;
 }
